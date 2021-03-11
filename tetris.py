7a7e4c213691338f4af6188ea10f056b72e8e7e5
thomashaaland <thomas.haaland@gmail.com>
2021-03-11 11:06:51 +0100

    Cleaned code

--- tetris.py
+++ tetris.py
@@ -132,27 +132,6 @@ class Tetramino(object):
                     if element == 1:
                         tetramino.append(Cube([self.pos[0] + (i_len-i)*self.increment,
                                                self.pos[1] + j*self.increment], self.sColor))
-        """
-        [[0, 1, 2, 3],
-         [4, 5, 6, 7],
-         [8, 9,10,11],
-         [12,13,14,15]]
-        
-        [[12, 8,4,0],
-         [13, 9,5,1],
-         [14,10,6,2],
-         [15,11,7,3]]
-
-        [[15, 14, 13, 12],
-         [11, 10,  9,  8],
-         [ 7,  6,  5,  4],
-         [ 3,  2,  1,  0]]
-
-        [[ 3, 7, 11, 15],
-         [ 2, 6, 10, 14],
-         [ 1, 5,  9, 13],
-         [ 0, 4,  8, 12]]
-        """
         return tetramino
 
     def draw(self, surface):
@@ -562,5 +541,6 @@ def main():
         pygame.display.update()
         pygame.display.flip()
 
-menu()
-#main()
+
+if __name__ == "__main__":
+    menu()

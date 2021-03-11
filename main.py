6c61ef7d5166c73e352ca10bd81a4707906127e8
thomashaaland <thomas.haaland@gmail.com>
2021-03-11 11:18:15 +0100

    Compatible with flask

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 from flask import Flask
+import tetris
 
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    tetris.menu()
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug = True)

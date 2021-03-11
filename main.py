5f0fcc9b0985bc517b78d1a2de78feba0b74cc3c
thomashaaland <thomas.haaland@gmail.com>
2021-03-11 13:04:58 +0100

    New initiation strategy

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    tetris.prep()
-    tetris.menu()
+    tetris.start_game()
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug = True)
